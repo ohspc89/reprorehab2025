50ef7d818a98728ead53902731add761d43aed76
bskillen <bskillen@ad.unc.edu>
2025-10-02 09:22:43 -0400

    Update Week1_assignment.py
    Finished the Assignment

--- contents/week1/Week1_assignment.py
+++ contents/week1/Week1_assignment.py
@@ -45,10 +45,16 @@ len(data)
 #3 days, I found this by looking at the key. Is there a way to programmatically 
 #do this?
 
+ 
+data['sub1']
+#day1, day2, day3
+
 # %% Task 4. What were the measures of each day?
 # hint: all three days were the same regarding measures
 
-#hours slept, coffee intake and mean cortisol levels
+data['sub1']['day1']
+
+#hour_slept, coffee_intake and mean_cortisol_levels
 
 # %% Task 5. Make a new dictionary, 'sleep_hours'.
 # Its structure will be like:
@@ -58,14 +64,18 @@ len(data)
 #    }
 # Task 5a: First create three lists, each of which will be the value
 # of 'sleep_hours'. Use list comprehensions.
+#describe a list comprehension
+#save each element of the list that you are looking for
 
-day1_value = 
-day2_value = ...
-day3_value = ...
+
+day1_value = [data[subject]['day1']['hour_slept'] for subject in data.keys()]
+day2_value = [data[subject]['day2']['hour_slept'] for subject in data.keys()]
+day3_value = [data[subject]['day3']['hour_slept'] for subject in data.keys()]
+
 
 # Task 5b: Now make 'sleep_hours'
 
-sleep_hours = ...
+sleep_hours = {'day1':day1_value, 'day2': day2_value, 'day3':day3_value}
 
 
 # %% Task 6. Calculate the mean and the standard deviation of hours slept
@@ -73,16 +83,20 @@ sleep_hours = ...
 # Make two variables ('means' and 'stds') using list comprehensions.
 # hint: import numpy and use `numpy.mean()` and `numpy.std()`
 
-import ...
-means = ...
-stds = ...
+import numpy as np
+
+
+means = {'day1_mean': np.mean(sleep_hours['day1']), 'day2_mean': np.mean(sleep_hours['day2']), 'day3_mean': np.mean(sleep_hours['day3'])}
+stds = {'day1_std': np.std(sleep_hours['day1']), 'day2_std': np.std(sleep_hours['day2']), 'day3_std': np.std(sleep_hours['day3'])}
 
 
 # %% Task 7. Plot daily sleep hour means using 'means' sequence prepared in Task 6.
 # Make sure that your days start from 1. What should you do then?
 # requirement: use `range()`
+x_days = range(1,4,1)
+mean_values = [means[day] for day in means]
 
 import matplotlib.pyplot as plt
-plt.plot(...)       # provide X and Y
-plt.xlabel(...)     # provide x-axis label
-plt.ylabel(...)     # provide y-axis label
+plt.plot(x_days, mean_values)       # provide X and Y
+plt.xlabel("Day")     # provide x-axis label
+plt.ylabel("Hours of Sleep")     # provide y-axis label
