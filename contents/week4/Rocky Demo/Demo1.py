64479218ed204bec5ab4492a8b54b0ee02e78440
Rocky Mazorow <rmazorow@stech.edu>
2025-10-29 10:44:08 -0500

    Update Demo1.py

--- contents/week4/Rocky Demo/Demo1.py
+++ contents/week4/Rocky Demo/Demo1.py
@@ -6,15 +6,15 @@ Created on Tue Oct 21 09:51:49 2025
 @author: rmazorow
 
 
-In this demo we will work with a 3D motion capture file. This file contains XYZ 
-motion capture data from 1 marker that was saved as 7 different markers. A new
-marker was generated in the data file everytime the true marker left the visual
-field and re-entered, therefore here are gaps in motion capture data.
+In this demo, we will work with a 3D motion capture file. This file contains 
+XYZ motion capture data from 1 marker that was saved as 7 different markers. A 
+new marker was generated in the data file every time the true marker left the 
+visual field and re-entered; therefore, there are gaps in motion capture data.
 
 First, we open the file, ignoring irrelevant headers. Second, we combine all 7 
 "markers" into one true marker. Third, we plot the updated results. Fourth, we 
 interpolate the data to fill in missing values. Last, we will explore different 
-mathmatical manipulations that can be done with position data.
+mathematical manipulations that can be done with position data.
 """
 
 # import packages
